2ed6d152efd07956b72b10d6c781cf838bb7e857
Alefh Sousa <alefh.sousa@gmail.com>
2023-03-09 21:17:34 -0300

    add create card mutation method

--- pipefy/api/cards.py
+++ pipefy/api/cards.py
@@ -1,5 +1,5 @@
 import uuid
-from typing import Optional
+from typing import Optional, List, Dict, Any
 
 from gql import gql
 
@@ -65,8 +65,35 @@ create_card_comment_mutation = """
 }
 """
 
+create_card_mutation = """
+    mutation createCard($pipe_id: ID!, $fields: [FieldValueInput],
+    $title: String, $identifier: String){
+      createCard(input: {
+        pipe_id: $pipe_id,
+        title: $title,
+        fields_attributes: $fields
+        clientMutationId: $identifier
+      }) {
+        card {
+          age
+          id
+        }
+      }
+    }
+"""
+
 
 class CardApi(BaseClient):
+
+    def create(self, pipe_id: str, title: Optional[str] = None, fields: Optional[List[Dict[str, Any]]] = None,
+               identifier: Optional[str] = None,
+               mutation: Optional[str] = None):
+        mutation = mutation or create_card_mutation
+        identifier = identifier or str(uuid.uuid4())
+        return self.client.api.fetch_data(
+            gql(mutation), dict(pipe_id=pipe_id, fields=fields, title=title, identifier=identifier)
+        )
+
     def load_card(self, card_id: str, query: Optional[str] = None):
         query = query or default_load_card_query
         return self.client.api.fetch_data(gql(query), {"id": card_id}).map(
